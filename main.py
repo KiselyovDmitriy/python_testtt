b2a938b828b2b4fc6909944ff4fcc1886e9ea0c6
KiselyovDmitriy <simon-000@mail.ru>
2024-06-28 21:09:51 +0300

    переменная

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-print("Всё получилось!!!")
+my_dict = {"Имя": "Алексей", "Год рождения": 1990}
+print(my_dict)
 
-x=5
-print(x)
+print(my_dict.get("Имя"))  # Выведет значение по ключу "Имя"
+print(my_dict.get("Профессия"))  # Выведет None без ошибки
+
+my_dict["Город"] = "Москва"
+my_dict["Хобби"] = "футбол"
+
+print(my_dict)
+
+value_deleted = my_dict.pop("Год рождения")
+print(value_deleted)
+
+print(my_dict)
