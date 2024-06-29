e2c1499089034b7aa075d5774daa15ba91b58dce
KiselyovDmitriy <simon-000@mail.ru>
2024-06-29 17:20:24 +0300

    операторы if, elif, else

--- main.py
+++ main.py
@@ -12,6 +12,7 @@
 first = input('Введите число: ')
 second = input('Введите число: ')
 third = input('Введите число: ')
+
 if first == second == third:
     print(3)
 elif first == second or first == third or second == third:
