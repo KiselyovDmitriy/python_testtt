81dc9f3a7c9f92abf98fce4d3c1c0609cf5c24e3
KiselyovDmitriy <simon-000@mail.ru>
2024-07-13 16:59:46 +0300

    цикл for

---
+++ module_2_4.py
@@ -0,0 +1,24 @@
+# Дан список чисел numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+# Используя этот список составьте второй список primes содержащий только простые числа.
+# А так же третий список not_primes, содержащий все не простые числа.
+# Выведите списки primes и not_primes на экран(в консоль).
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+primes = []
+not_primes = []
+
+for num in numbers:
+    if num > 1:
+        is_prime = True
+        for i in range(2, num):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(num)
+        else:
+            not_primes.append(num)
+
+print("Простые числа:", primes)
+print("Не простые числа:", not_primes)
--- probe.py
+++ probe.py
@@ -1,4 +1,39 @@
-for i in range(1, 11):
-    for j in range(1, 11):
-        print(f"{i} x {j} = {i * j}")
+# for i in range(1, 11):
+#     for j in range(1, 11):
+#         print(f"{i} x {j} = {i * j}")
+#
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+# primes = []
+# not_primes = []
+#
+# for number in numbers:
+#     if number > 1:
+#         for i in range(2, number):
+#             if (number % i) == 0:
+#                 not_primes.append(number)
+#                 break
+#         else:
+#             primes.append(number)
+#
+# print("Простые числа:", primes)
+# print("Не простые числа:", not_primes)
 
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+primes = []
+not_primes = []
+
+for num in numbers:
+    if num > 1:
+        is_prime = True
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(num)
+        else:
+            not_primes.append(num)
+
+print("Простые числа:", primes)
+print("Не простые числа:", not_primes)
